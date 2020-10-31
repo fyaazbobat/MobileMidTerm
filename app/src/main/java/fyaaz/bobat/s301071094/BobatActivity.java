bc53c64f9b7a66290f07b8539dbc18a96a3f0371
fbobat <71932173+fbobat@users.noreply.github.com>
2020-10-31 10:28:29 -0400

    worked on first and second layout

--- app/src/main/java/fyaaz/bobat/s301071094/BobatActivity.java
+++ app/src/main/java/fyaaz/bobat/s301071094/BobatActivity.java
@@ -1,5 +1,7 @@
 package fyaaz.bobat.s301071094;
+// 301071094 Fyaaz Bobat
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BobatActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BobatActivity extends Fragment {
+public class BobatActivity extends Activity {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,23 +45,20 @@ public class BobatActivity extends Fragment {
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bobat_activity, container, false);
+
+
     }
 }
--- app/src/main/java/fyaaz/bobat/s301071094/FyaazActivity.java
+++ app/src/main/java/fyaaz/bobat/s301071094/FyaazActivity.java
@@ -1,14 +1,46 @@
 package fyaaz.bobat.s301071094;
+// 301071094 Fyaaz Bobat
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class FyaazActivity extends AppCompatActivity {
+public class FyaazActivity extends Activity {
+    Button submit;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.fyaTextPass);
+        password = (EditText)findViewById(R.id.fyaNum);
+        submit = (Button) findViewById(R.id.fyabutton);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
+    }
+
+    public void Login()
+    {
+        if(username.getText().toString().equals("fyaaz.bobat") && password.getText().toString().equals("301071094")){
+            Intent intent = new Intent(this,BobatActivity.class);
+            startActivity(intent);
+        //correcct password
+    }else{
+        //wrong password
+    }
     }
 }
