2967d5752acaaa78e8105dfcc67cdaad7d1feba0
fbobat <71932173+fbobat@users.noreply.github.com>
2020-10-31 08:42:24 -0400

    initial commit

---
+++ app/src/main/java/fyaaz/bobat/s301071094/FyaazActivity.java
@@ -0,0 +1,14 @@
+package fyaaz.bobat.s301071094;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FyaazActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
